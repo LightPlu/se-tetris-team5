ce81958552abb9ea472838e5734b9b293007911e
배재훈 <lange50300@gmail.com>
2025-12-03 00:22:26 +0900

    fix: 버튼 이미지 수정

--- app/src/main/java/se/tetris/team5/screens/home.java
+++ app/src/main/java/se/tetris/team5/screens/home.java
@@ -53,7 +53,7 @@ public class home extends JPanel implements KeyListener {
     };
     
     private String[] mainMenuIcons = {
-        "🎮", "💎", "⚔️", "🌐", "🏆", "⚙️", "❌"
+        "🎮", "💎", "⚡", "🌐", "🏆", "⚙", "✖"
     };
     
     private String[] mainMenuDescriptions = {
@@ -94,7 +94,7 @@ public class home extends JPanel implements KeyListener {
     };
     
     private String[] battleModeIcons = {
-        "⚔️", "💎⚔️", "⏱️", "↩️"
+        "⚡", "💎⚡", "⏰", "←"
     };
     
     private String[] battleModeDescriptions = {
